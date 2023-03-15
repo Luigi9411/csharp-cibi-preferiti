962f66c70a95985800b32c0b98bb9c51540ad6b2
Luigi9411 <luigi.sciavicco@gmail.com>
2023-03-15 15:55:32 +0100

    Aggiungere i file di progetto.

---
+++ CibiPreferiti/Program.cs
@@ -0,0 +1,20 @@
+﻿string[] food = { "Pizza", "Pasta", "Gelato", "Polpette", "Frittura di pesce", "Lenticchie", "Fagioli", "Spiedini", "Salmone", "Pane" };
+Console.WriteLine(food.Length);
+
+for (var i = 0; i < food.Length; i++)
+{
+    Console.Write(i);
+    Console.WriteLine( food[i].PadLeft(20));
+};
+
+    Console.WriteLine("Il cibo preferito è:");
+    Console.WriteLine(food[0]);
+
+Console.WriteLine("Ultima posizione in classifica:");
+Console.WriteLine(food[9]);
+
+Console.WriteLine("Cibi di metà classifica:");
+Console.WriteLine(food[5]);
+Console.WriteLine(food[6]);
+
+
